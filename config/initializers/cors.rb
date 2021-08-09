216416febac5e528d1c90b2f868a286fef885770
shida22553 <53365248+shida22553@users.noreply.github.com>
2021-08-09 18:39:31 +0900

    set cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://shida22553.github.io/'
+    origins '*'
 
     resource '*',
              headers: :any,
